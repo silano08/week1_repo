ce72c76616f87407268a7143f65dc3284ac6bdf6
Sung Mok Jung <newyear94@naver.com>
2021-03-04 03:54:02 +0900

    EDIT sungmok

--- yihyeon/app.py
+++ yihyeon/app.py
@@ -2,9 +2,10 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 
 from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -15,19 +16,53 @@ client = MongoClient('localhost', 27017)
 db = client.dbsparta_team_project01
 
 # html파일 불러오기
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 # 회원가입 페이지 불러오기
+
+
 @app.route('/sign_up')
 def sign_up():
     return render_template('signup.html')
 
 # 택배조회 페이지 불러오기
+
+
 @app.route('/show_mylist')
 def show_mylist():
-    return render_template('mylist.html')
+    # 토큰 꺼내오기
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_id = db.users.find_one({"username": payload["id"]})['username']
+        print(user_id)
+        return render_template('mylist.html', user_info=user_id)
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+# 택배조회 페이지에 아이디 던져주기
+# @app.route('/show_mylist', methods=['POST'])
+# def get_username():
+#
+#     return jsonify({'result': 'success'})
+
+#
+# @app.route('/result', methods=['GET'])
+# def result():
+#     # user_id = request.form['userid']
+#     user_id = request.args.get('userid')
+#
+#     return render_template('mylist.html', user_id)
+
 
 # 회원가입
 @app.route('/sign_up/register', methods=['POST'])
@@ -35,7 +70,8 @@ def register():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    password_hash = hashlib.sha256(
+        password_receive.encode('utf-8')).hexdigest()
 
     doc = {
         "username": username_receive,
@@ -45,7 +81,7 @@ def register():
     return jsonify({'result': 'success'})
 
 
-#중복확인
+# 중복확인
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
@@ -65,7 +101,8 @@ def sign_in():
     # 비밀번호 암호화
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     # db에 데이터 넣고 조회 한 결과
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+    result = db.users.find_one(
+        {'username': username_receive, 'password': pw_hash})
 
     # 회원인 경우
     if result is not None:
